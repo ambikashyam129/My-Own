1ce78bcfc53b3373eca0a3daf57a7e9816b6392e
ambikashyam129 <41755171+ambikashyam129@users.noreply.github.com>
2018-10-19 08:28:04 +0530

    c program

---
+++ evenodd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+int n,rem;
+printf("Enter the number\n");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("Number is even\n");
+}
+if(n%2!=0)
+{
+printf("Number is odd\n");
+}
+}
