eb3d734c41afb773bc02a68ba1ac32ee6f7432c9
ambikashyam129 <41755171+ambikashyam129@users.noreply.github.com>
2018-10-20 23:37:18 +0530

    Add files via upload

---
+++ fib.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+int f1=0,f2=1,f3,i=3,length;
+printf("Enter the length of the fibonocci\n");
+scanf("%d",&length);
+printf("%d %d",f1,f2);
+while(i<=length)
+{
+f3=f1+f2;
+printf("%d",f3);
+f1=f2;
+f2=f3;
+i=i+1;
+}
+}
+
